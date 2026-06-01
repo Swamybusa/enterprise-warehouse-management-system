0af9602c1d989a2ce4a3396b26135c49008ce663
Swamybusa <you@example.com>
2026-06-01 18:31:28 +0530

    Added authentication controller with JWT login

---
+++ enterprise-warehouse-management-system/src/main/java/com/infotact/enterprise_warehouse_management_system/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.infotact.enterprise_warehouse_management_system.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.infotact.enterprise_warehouse_management_system.dto.AuthRequest;
+import com.infotact.enterprise_warehouse_management_system.model.JwtUtil;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest request) {
+
+        // TEMP login (we will connect DB later)
+        if ("admin".equals(request.getUsername())
+                && "admin".equals(request.getPassword())) {
+
+            return jwtUtil.generateToken(request.getUsername());
+        }
+
+        return "Invalid credentials";
+    }
+}
