c86ba06e226be872a2e4da98cbc5c099648c9ad0
Swamybusa <you@example.com>
2026-06-01 18:29:14 +0530

    Implemented JWT Token generation utility

---
+++ enterprise-warehouse-management-system/src/main/java/com/infotact/enterprise_warehouse_management_system/model/JwtUtil.java
@@ -0,0 +1,30 @@
+package com.infotact.enterprise_warehouse_management_system.model;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET =
+            "mysecretkeymysecretkeymysecretkeymysecretkey";
+
+    private final SecretKey key =
+            Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(key)
+                .compact();
+    }
+}
